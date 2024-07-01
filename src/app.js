48210fe13e1a69368d7612e4c87763c75853beab
Alejandro Gonzalez <ajgonzale@gmail.com>
2024-07-01 20:16:41 +0000

    generate the random cards

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function() {};
+
+function createCard() {
+  let suits = ["♦", "♥", "♠", "♣"];
+  let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+  let number = numbers[Math.floor(Math.random() * numbers.length)];
+  let color = "";
+  if (suits.indexOf(suit) < 3) color = "text-danger";
+
+  let html = `<div class="col-12 text-start spade ${color}">
+            ${suit}
+          </div><div class="col-12 number text-center ${color}">
+            ${number}
+          </div><div class="col-12 text-end spade backwards ${color}">
+            ${suit}
+          </div>`;
+
+  let div = document.createElement("div");
+  div.classList.add("card");
+
+  div.innerHTML = html;
+  let cards = document.querySelector(".cards");
+  cards.appendChild(div);
+}
+window.createCards = function createCards() {
+  let number = document.querySelector("#numberCard");
+
+  for (let i = 0; i < number.value; i++) {
+    createCard();
+  }
 };
