4145093949069241903c17c440c0f7ec99c5010a
Alejandro Gonzalez <ajgonzale@gmail.com>
2024-07-01 22:11:11 +0000

    complete the exercise4

--- src/app.js
+++ src/app.js
@@ -7,14 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {};
 
+var cardsArray = [];
+var contLogs = 0;
+
 function createCard() {
   let suits = ["♦", "♥", "♠", "♣"];
-  let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+  let cardValue = 0;
 
   let suit = suits[Math.floor(Math.random() * suits.length)];
   let number = numbers[Math.floor(Math.random() * numbers.length)];
   let color = "";
-  if (suits.indexOf(suit) < 3) color = "text-danger";
+  if (suits.indexOf(suit) < 2) color = "text-danger";
+
+  cardValue = numbers.indexOf(number);
+
+  //Spade < hearst < diamons < club;
+  switch (suits.indexOf(suit)) {
+    //diamons
+    case 0:
+      cardValue += 200;
+      break;
+    //hearts
+    case 1:
+      cardValue += 100;
+      break;
+    //spade
+    case 2:
+      break;
+    //club
+    case 3:
+      cardValue += 300;
+      break;
+  }
 
   let html = `<div class="col-12 text-start spade ${color}">
             ${suit}
@@ -30,11 +56,98 @@ function createCard() {
   div.innerHTML = html;
   let cards = document.querySelector(".cards");
   cards.appendChild(div);
+  return cardValue;
 }
+
 window.createCards = function createCards() {
+  cardsArray = [];
+  let cards = document.querySelector(".cards");
+  cards.innerHTML = "";
+  contLogs = 0;
   let number = document.querySelector("#numberCard");
 
   for (let i = 0; i < number.value; i++) {
-    createCard();
+    cardsArray.push(createCard());
   }
+};
+
+const selectSort = arr => {
+  let min = 0;
+  while (min < arr.length - 1) {
+    for (let i = min + 1; i < arr.length; i++) {
+      if (arr[min] > arr[i]) {
+        let aux = arr[min];
+        arr[min] = arr[i];
+        arr[i] = aux;
+      }
+    }
+    print(arr);
+    contLogs++;
+    min++;
+  }
+  return arr;
+};
+
+function print(arr) {
+  let html = `<div class="logs${contLogs} d-flex">${contLogs}</div>`;
+  let div = document.createElement("div");
+
+  div.innerHTML = html;
+  let divClass = ".logs";
+  let cards = document.querySelector(divClass);
+  cards.appendChild(div);
+  divClass = ".logs" + contLogs;
+  for (let i = 0; i < arr.length; i++) {
+    createCardByValue(arr[i], divClass);
+  }
+}
+
+function createCardByValue(value, divId) {
+  let suits = ["♦", "♥", "♠", "♣"];
+  let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+  let suit = "";
+  let color = "";
+
+  console.log("f", value);
+  if (value < 100) {
+    suit = suits[2];
+  } else if (value < 200) {
+    suit = suits[1];
+    value -= 100;
+    color = "text-danger";
+  } else if (value < 300) {
+    suit = suits[0];
+    value -= 200;
+    color = "text-danger";
+  } else {
+    suit = suits[3];
+    value -= 300;
+  }
+
+  let number = numbers[value];
+  let html = `<div class="col-12 text-start spade ${color}">
+            ${suit}
+          </div><div class="col-12 number text-center ${color}">
+            ${number}
+          </div><div class="col-12 text-end spade backwards ${color}">
+            ${suit}
+          </div>`;
+
+  let div = document.createElement("div");
+  div.classList.add("card");
+
+  div.innerHTML = html;
+  let cards = document.querySelector(divId);
+
+  cards.appendChild(div);
+}
+
+window.sort = function sort() {
+  let divClass = ".logs";
+  let cards = document.querySelector(divClass);
+  cards.innerHTML = "";
+  cardsArray = selectSort(cardsArray);
+
+  console.log(cardsArray);
 };
